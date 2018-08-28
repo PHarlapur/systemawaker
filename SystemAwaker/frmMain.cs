7181545a9be7e6b2bea55a0db632df5c8c4b3902
Prabhas HARLAPUR <pharlapur@markem-imaje.com>
2018-08-28 11:59:54 +0530

    Created new System Awaker program to keep the system active when it is needed most.

---
+++ SystemAwaker/frmMain.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SystemAwaker
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //Enable the awake mode
+            PowerHelper.ForceSystemAwake();
+            btnStart.Enabled = false;
+        }
+
+        private void btnEnd_Click(object sender, EventArgs e)
+        {
+            PowerHelper.ResetSystemDefault();
+            btnStart.Enabled = true;
+
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            PowerHelper.ResetSystemDefault();
+            btnStart.Enabled = true;
+        }
+
+        private void frmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            PowerHelper.ResetSystemDefault();
+            btnStart.Enabled = true;
+        }
+    }
+
+
+    public class PowerHelper
+    {
+        public static void ForceSystemAwake()
+        {
+            NativeMethods.SetThreadExecutionState(NativeMethods.EXECUTION_STATE.ES_CONTINUOUS |
+                                                  NativeMethods.EXECUTION_STATE.ES_DISPLAY_REQUIRED |
+                                                  NativeMethods.EXECUTION_STATE.ES_SYSTEM_REQUIRED |
+                                                  NativeMethods.EXECUTION_STATE.ES_AWAYMODE_REQUIRED);
+        }
+
+        public static void ResetSystemDefault()
+        {
+            NativeMethods.SetThreadExecutionState(NativeMethods.EXECUTION_STATE.ES_CONTINUOUS);
+        }
+    }
+
+    internal static partial class NativeMethods
+    {
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern EXECUTION_STATE SetThreadExecutionState(EXECUTION_STATE esFlags);
+
+        [FlagsAttribute]
+        public enum EXECUTION_STATE : uint
+        {
+            ES_AWAYMODE_REQUIRED = 0x00000040,
+            ES_CONTINUOUS = 0x80000000,
+            ES_DISPLAY_REQUIRED = 0x00000002,
+            ES_SYSTEM_REQUIRED = 0x00000001
+
+            // Legacy flag, should not be used.
+            // ES_USER_PRESENT = 0x00000004
+        }
+    }
+}
